4511ab9b346c169bba8b14572f8f38d270c23b98
Han <han@u29dc.com>
2026-01-16 12:08:55 +0000

    chore(repo): add notion domains and simplify lint-staged config
    - Add notion.com and developers.notion.com to allowed WebFetch domains
    - Simplify lint-staged to trigger single util:check for all files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,3 @@
 export default {
-	'*.{js,ts,json,jsonc,md}': ['biome format --write', 'biome check --write'],
-	'*.sh': ['shfmt -w -i 4 -ci', 'shellcheck'],
+	'*': () => ['bun run util:check'],
 };
